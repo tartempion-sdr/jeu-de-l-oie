54368aab70219e0bc6065e3c9a8532d172ab4cf3
Tartempion <mammuth-4639@gmail.com>
2023-03-04 20:53:51 +0100

    init

---
+++ READ ME
@@ -0,0 +1,10 @@
+jeu de l'oie read me
+
+nb de joueur max 4
+
+ramdom dé
+
+case 
+
+ia
+
---
+++ jeudeloie.py
@@ -0,0 +1,22 @@
+import pygames
+import ramdom
+
+
+nbJoueursMax = 4
+nbJoueurs = int()
+nomJoueur = ""
+aQuiLeTour = ""
+vinqueur = ""
+position = []
+pion = []
+
+
+def ia(): 
+    ia
+grille = []
+
+def lancerlede():
+    random
+
+def entrez_votre_nom():
+    print(input("veuillez entrer votre nom"))
