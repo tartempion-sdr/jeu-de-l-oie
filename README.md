fbc33e211938f40ea8ae43970423e3af0d370e96
Tartempion <mammuth-4639@gmail.com>
2023-03-06 02:07:53 +0100

    ok

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 jeu de l'oie read me
 
+a règle de base est intangible. Le jeu se joue avec 2 dés. Un premier coup décide de celui qui va commencer. L'oie signale les cases fastes disposées de 9 en 9. Nul ne peut s'arrêter sur ces cases bénéfiques et on double alors le jet.
+
+    Qui fait 9 au premier jet, ira au 26 s'il l'a fait par 6 et 3, ou au 53 s'il l'a fait par 4 et 5.
+    Qui tombe à 6, où il y a un pont, ira à 12.
+    Qui tombe à 19, où il y a un hôtel, se repose quand chacun joue 2 fois.
+    Qui tombe à 31, où il y a un puits attend qu'on le relève.
+    Qui tombe à 42, où il y a un labyrinthe retourne à 304.
+    Qui tombe à 52, où il y a une prison attend qu'on le relève.
+    Qui tombe à 58, où il y a la mort, recommence.
+
+Le premier arrivé à 63, dans le jardin de l'oie, gagne la partie. À condition de tomber juste, sinon il retourne en arrière, sur autant de cases qu'il lui reste à parcourir.
+
+Si un joueur tombe sur une case déjà occupée par un autre joueur il renvoie ce dernier à la case d'où il est parti. Il ne peut y avoir qu'un joueur par case. 
+
+
+valeur au debut: 
+
 nb de joueur max 4
-
 ramdom dé
-
 case 
-
 ia
+
+nom
 
--- jeudeloie.py
+++ jeudeloie.py
@@ -1,22 +1,69 @@
-import pygames
-import ramdom
+import pygame
+import random
+from pynput.keyboard import  Listener, Key
+from pynput.keyboard import Key
+from pynput import *
+
+class Parametres:    
+    """
+     blabla
+    """
+    def __init__(self):
+        
+        
+        
+        self.nomjoueur = ""
+        self.position = int()
+        self.grille = list
+        self.valeurdude = int()
+
+    def entrez_votre_nom(self):
+        
+        while self.nomjoueur == str(""):
+       
+            self.nomjoueur = input("veuillez entrer votre nom : ")
+            if self.nomjoueur != str(""):
+                print("Vous Vous appellez : " + self.nomjoueur)
+                print("\n" + self.nomjoueur + " appuyer sur la touche a pour lancé le dé\n")
+    
+    def lancerlede(self):
+        
+        def on_press(key):
+            
+            if key.char ==  "a" :     
+                J1.valeurdude = J1.printrandom()
+                pass
+                    
+            
+        
+        with Listener(on_press=on_press, ) as listener:
+
+            listener.join()    
+        
+    def printrandom(self):
+            
+            de = str(random.randint(1, 6))
+            deresultat = de
+            print(J1.valeurdude)
+            return deresultat 
+                
+     
+        
+    
+
+J1 = Parametres()
+
+J1.entrez_votre_nom()
+
+J1.lancerlede()
 
 
-nbJoueursMax = 4
-nbJoueurs = int()
-nomJoueur = ""
-aQuiLeTour = ""
-vinqueur = ""
-position = []
-pion = []
 
 
-def ia(): 
-    ia
-grille = []
 
-def lancerlede():
-    random
 
-def entrez_votre_nom():
-    print(input("veuillez entrer votre nom"))
+
+    
+
+
+
